fa3be533e1a99a407cfddff1f47f88891d2c7d91
D <denise.meerkerk@gmail.com>
2021-05-26 18:01:43 +0200

    better train/test/val split

--- preprocessing.py
+++ preprocessing.py
@@ -40,7 +40,7 @@ def split_all_soundfiles(part=1, segment=0):
     wav_files, anno_files = code_snippets.getAudioFilenames()
     X,y = [],[]
     filenumbers = range(int(segment*len(wav_files)*part), 
-                        int(segment*len(wav_files)*part+len(wav_files)*part))
+                        min(int(segment*len(wav_files)*part+len(wav_files)*part),len(wav_files)))
     for i in filenumbers:
         Xtemp, ytemp = split_soundfile(file_number=i,wav_files=wav_files,
                                        anno_files=anno_files)
@@ -51,8 +51,10 @@ def split_all_soundfiles(part=1, segment=0):
     
     # remove weird annotations
     weird = list(set(y))
-    weird.remove('?')
-    weird.remove('.')
+    if '?' in weird:
+        weird.remove('?')
+    if '.' in weird:
+        weird.remove('.')
     
     indices = [i for i, x in enumerate(y) if x in weird]
     
@@ -62,6 +64,26 @@ def split_all_soundfiles(part=1, segment=0):
     return X, y
         
 
+
+def train_test_val_split(train_part=0.6, test_val_part=0.2):
+    train_segment=(1-train_part)/train_part/2
+    X_train, y_train = split_all_soundfiles(part=train_part, segment=train_segment)
+    #print('train complete')
+    test_segment1=0
+    test_segment2=(2/test_val_part)-1
+    X_test1, y_test1 = split_all_soundfiles(part=test_val_part, segment=test_segment1)
+    X_test2, y_test2 = split_all_soundfiles(part=test_val_part, segment=test_segment2)
+    X_test = X_test1 + X_test2
+    y_test = y_test1 + y_test2
+    #print('test complete')
+    X_val1, y_val1 = split_all_soundfiles(part=test_val_part, segment=test_segment1+1)
+    X_val2, y_val2 = split_all_soundfiles(part=test_val_part, segment=test_segment2-1)
+    X_val = X_val1 + X_val2
+    y_val = y_val1 + y_val2
+    #print('val complete')
+    return X_train, y_train, X_test, y_test, X_val, y_val
+#X_train, y_train, X_test, y_test, X_val, y_val = train_test_val_split()
+    
 #%% quick fix to pad the data.
 def pad_data(X):
 
