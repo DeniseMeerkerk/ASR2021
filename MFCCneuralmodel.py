2bb7a785e978a9280c08be3c8880fa9fac5c1cd0
D <denise.meerkerk@gmail.com>
2021-05-26 19:31:02 +0200

    update mfcc neural network

--- MFCCneuralmodel.py
+++ MFCCneuralmodel.py
@@ -26,9 +26,9 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 #%% audio input
 
-def getMFCCData(part=1, segment=0):
+def getMFCCData(X):
     # X,y = preprocessing.split_soundfile(-1) # one data file
-    X,y = preprocessing.split_all_soundfiles(part,segment) # 20% of data files
+    #X,y = preprocessing.split_all_soundfiles(part,segment)
     
     sample_rate = 16000 # check this later
     feature_dim_1 = 20 # check this: n_mfcc=20
@@ -38,14 +38,14 @@ def getMFCCData(part=1, segment=0):
         mfcc_X.append(librosa.feature.mfcc(y=x, sr=sample_rate, S=None,
                                            n_mfcc=feature_dim_1, dct_type=2,
                                            norm='ortho', lifter=0))
-    return mfcc_X, y
+    return mfcc_X
 
 #2d padding
 def MFCC_padding(mfcc_X):
     new_mfcc_X = mfcc_X.copy()
     new_mfcc_X.sort(key=np.shape, reverse=True)
     feature_dim_1 = mfcc_X[0].shape[0] # check this: n_mfcc=20
-    feature_dim_2 = new_mfcc_X[0].shape[1] 
+    feature_dim_2 = 1260 #new_mfcc_X[0].shape[1] 
         
     xxx = np.zeros([len(mfcc_X),feature_dim_1,feature_dim_2])
     for n, dingen in enumerate(mfcc_X):
@@ -63,19 +63,25 @@ def prepareMFCCdata(channel=1,part=1, segment=0):
     encoder = LabelEncoder()
     y = encoder.fit_transform(y)
     
-    X_train, X_test, y_train, y_test = train_test_split(xxx, y, test_size= 0.2,
+    X_train, X_test, y_train, y_test = train_test_split(xxx, y, test_size= 0.4,
                                                     random_state=True,
                                                     shuffle=True)
+    X_val, X_test, y_val, y_test = train_test_split(xxx, y, test_size= 0.5,
+                                                    random_state=True,
+                                                    shuffle=True)
+    
     # Reshaping to perform 2D convolution
     X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
     X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+    X_val = X_val.reshape(X_val.shape[0], feature_dim_1, feature_dim_2, channel)
     
     y_train = to_categorical(y_train)
     y_test = to_categorical(y_test)
+    y_val = to_categorical(y_val)
     
     class_weights = compute_class_weight('balanced',[0,1], y=y)
     
-    return X_train, X_test, y_train, y_test, feature_dim_1, feature_dim_2, class_weights
+    return X_train, X_test, X_val, y_train, y_test, y_val, feature_dim_1, feature_dim_2, class_weights
 
 
 def modelParameters():
@@ -107,12 +113,13 @@ def get_model(channel,feature_dims,num_classes):
     return model
 
 def evaluate(model, X_test, y_test, batch_size=32):
-    print("Evaluate on test data")
-    results = model.evaluate(X_test, y_test, batch_size=32)
+    print("Evaluation")
+    loss, acc = model.evaluate(X_test, y_test, batch_size=32)
     y_test_pred = model.predict(X_test)
     cm=confusion_matrix(np.argmax(y_test,axis=1),np.argmax(y_test_pred,axis=1))
     print('confusion matrix:\n',cm)
-    print("test loss, test acc:", results)
+    print("accuracy: {:5.2f}%".format(100 * acc))
+    results = [loss,acc]
     return results, cm
 
 
@@ -124,24 +131,52 @@ def load_test_model(part=1):
     results,cm = evaluate(model, X_train, y_train)
     return
 
+def MFCC_data(X_train, y_train, X_test, y_test, X_val, y_val, channel=1):
+    #X data
+    X_train = getMFCCData(X_train)
+    X_train,feature_dim_1, feature_dim_2 = MFCC_padding(X_train)
+    X_train = X_train.reshape(X_train.shape[0], feature_dim_1, feature_dim_2, channel)
+    X_test = getMFCCData(X_test)
+    X_test,feature_dim_1, feature_dim_2 = MFCC_padding(X_test)
+    X_test = X_test.reshape(X_test.shape[0], feature_dim_1, feature_dim_2, channel)
+    X_val = getMFCCData(X_val)
+    X_val,feature_dim_1, feature_dim_2 = MFCC_padding(X_val)
+    X_val = X_val.reshape(X_val.shape[0], feature_dim_1, feature_dim_2, channel)
+    
+    
+    #y labels
+    encoder = LabelEncoder()
+    y = y_train + y_test + y_val
+    y = encoder.fit_transform(y)
+    class_weights = compute_class_weight('balanced', [0, 1], y=y)
+    del y
+    
+    y_train = encoder.fit_transform(y_train)
+    y_test = encoder.fit_transform(y_test)
+    y_val = encoder.fit_transform(y_val)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    y_val = to_categorical(y_val)
+    
+    return X_train, X_test, X_val, y_train, y_test, y_val, feature_dim_1, feature_dim_2, class_weights
+
 #%% 
 
 def main():
-    part = 0.4 # should be >=0.1
+    # preprocess data
+    X_train, y_train, X_test, y_test, X_val, y_val = preprocessing.train_test_val_split(0.1,0.1)
     channel, epochs, batch_size, verbose, num_classes = modelParameters()
-    X_train, X_test, y_train, y_test, feature_dim_1, feature_dim_2, class_weights = prepareMFCCdata(channel,part=part)
-    X_val = X_train[-len(y_test):,:,:,:]
-    y_val = y_train[-len(y_test):,:]
-    X_train = X_train[:-len(y_test),:,:,:]
-    y_train = y_train[:-len(y_test),:]
-    model = get_model(channel,[feature_dim_1,feature_dim_2],num_classes)
     
+    X_train, X_test, X_val, y_train, y_test, y_val, feature_dim_1, feature_dim_2, class_weights = MFCC_data(X_train, y_train, X_test, y_test, X_val, y_val)
+    
+    #initiate model
+    model = get_model(channel,[feature_dim_1,feature_dim_2],num_classes)
     optimizer = optimizers.SGD(lr=0.002, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss=keras.losses.binary_crossentropy,optimizer=optimizer,
                   metrics=['accuracy'])
     
-    checkpoint_path = "training_1/cp.ckpt"
-    checkpoint_dir = os.path.dirname(checkpoint_path)
+    #callbacks
+    checkpoint_path = "training_05-26/cp.ckpt"
     
     earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0,
                                   mode='min')
@@ -152,7 +187,7 @@ def main():
                                        patience=7, verbose=1, epsilon=1e-4, 
                                        mode='min')
 
-    
+    # fit model; best weights are saved in variable ```checkpoint_path``` folder
     model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, 
               verbose=verbose, callbacks=[earlyStopping, mcp_save, reduce_lr_loss],
               validation_data=(X_val, y_val),
@@ -163,19 +198,16 @@ def main():
     model2.compile(loss=keras.losses.binary_crossentropy,optimizer=optimizer,
                   metrics=['accuracy'])
     
-    # Evaluate the model
+    # Evaluate the untrained model
+    print("Untrained model test set results")
     loss, acc = model2.evaluate(X_test, y_test, verbose=2)
-    print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+    results,cm = evaluate(model2, X_test, y_test)
 
     # Loads the weights
     model2.load_weights(checkpoint_path)
-    
-    # Re-evaluate the model
-    loss, acc = model2.evaluate(X_test, y_test, verbose=2)
-    print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
-
-    
-    #results,cm = evaluate(model, X_test, y_test)
+    # Re-evaluate the model  
+    print("Trained model test set results")
+    results,cm = evaluate(model2, X_test, y_test)
     return
 
 #%%
